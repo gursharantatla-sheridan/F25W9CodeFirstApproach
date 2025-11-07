c258daad9f1f300178bf101b491776bd85c04c96
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-11-07 15:14:04 -0500

    data annotations

--- F25W9CodeFirstApproach/Migrations/20251107200141_AddAgeToStudent.Designer.cs
+++ F25W9CodeFirstApproach/Migrations/20251107200141_AddAgeToStudent.Designer.cs
@@ -3,6 +3,7 @@ using F25W9CodeFirstApproach;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace F25W9CodeFirstApproach.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20251107200141_AddAgeToStudent")]
+    partial class AddAgeToStudent
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -79,8 +82,7 @@ namespace F25W9CodeFirstApproach.Migrations
                         .HasColumnType("int");
 
                     b.Property<string>("StudentName")
-                        .HasColumnType("nvarchar(max)")
-                        .HasColumnName("Name");
+                        .HasColumnType("nvarchar(max)");
 
                     b.HasKey("StudentId");
 
