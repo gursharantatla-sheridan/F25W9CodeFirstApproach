3ac6da619bcdc7883fcd385c8e33febec6aaae64
Gursharan Singh Tatla <sing1731@shernet.sheridancollege.ca>
2025-11-07 13:30:56 -0500

    Add project files.

---
+++ F25W9CodeFirstApproach/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace F25W9CodeFirstApproach
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
